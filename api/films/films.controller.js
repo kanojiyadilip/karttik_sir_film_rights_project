55548f2a84bdd1229fd96377d6b290f193a0550b
dilip1093 <dilip.1093@gmail.com>
2023-03-24 00:19:34 +0530

    pm2 restart server command test again

--- api/films/films.controller.js
+++ api/films/films.controller.js
@@ -200,13 +200,7 @@ function createFilmRight(req,res){
 
 
 function createUser(req,res){
-    const exec = require("child_process").exec
-    exec("pm2 restart all", (error, stdout, stderr) => {
-    console.log("===error==>",error);
-    console.log("===stdout==>",stdout);
-    console.log("===stderr==>",stderr);
-    //do whatever here
-    })
+
     // console.log("-<req>-", req);
    console.log("-<req f. files>-", req.files);
     if (req.files == undefined) {
@@ -219,6 +213,13 @@ function createUser(req,res){
     else {
 
         filmService.createUser(req, function (result) {
+            const exec = require("child_process").exec
+            exec("pm2 restart all", (error, stdout, stderr) => {
+            console.log("===error==>",error);
+            console.log("===stdout==>",stdout);
+            console.log("===stderr==>",stderr);
+            //do whatever here
+            })
             // if error is there
             if (result instanceof Error) {
                 res.status(200).json(result)
